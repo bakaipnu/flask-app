80cd66ee4b699eb563961828cb7d2e4534b5b869
dxsw <yurabakai2.0@gmail.com>
2024-10-26 13:06:08 +0200

    feat: added template files

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 from utils.config import get
@@ -9,7 +9,17 @@ app = Flask(get("FLASK_APP"))
 
 @app.route("/")
 def main():
-    return "Hello, world!"
+    return render_template("home.html", page_title="Головна")
+
+
+@app.route("/resume")
+def resume():
+    return render_template("resume.html", page_title="Моє резюме")
+
+
+@app.route('/contact')
+def contact():
+    return render_template('contacts.html', page_title="Контакти")
 
 
 if __name__ == "__main__":
